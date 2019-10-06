f7a0a73e1772c79327ae92b9e576a833bc16fbe7
Owen Chang <owenchang@m-toy.com.tw>
2019-10-06 22:59:03 +0800

    add search function

--- read.py
+++ read.py
@@ -4,29 +4,59 @@ with open ('reviews.txt' , 'r') as f:
 	for line in f:
 		data.append(line)
 		count = count + 1
-		#if count % 1000 == 0:
+		#if count % 1000 == 0: 
 			#print(len(data))
 print('檔案讀取完了,總共有', len(data), '筆資料')
 
-sum_len = 0
+print(data[0])
+
+wc = {} #word_count
 for d in data:
-	sum_len = sum_len + len(d)
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1 # wc[are] = 到wc字典內查are這個字
+		else:
+			wc[word] = 1 #新增新的key進字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+
+
+while True:
+	word = input('請問您想查什麼字？')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為：', wc[word])
+	else:
+		print('0')
+print('感謝使用本查詢功能')
+
+
+
+
+#sum_len = 0
+#for d in data:
+#	sum_len = sum_len + len(d)
 	#print(sum_len)
 
-print('平均是', sum_len/len(data), '字數')
+#print('平均是', sum_len/len(data), '字數')
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '留言小於一百個字')
-print(new[0])
+#new = []
+#for d in data:
+#	if len(d) < 100:
+#		new.append(d)
+#print('一共有', len(new), '留言小於一百個字')
+#print(new[0])
 
 
-good = []
-good_count = 0
-for d in data:
-	if 'good' in d:
-		good.append(d)
-		good_count = good_count + 1
-print(good_count)
+#good = []
+#good_count = 0
+#for d in data:
+#	if 'good' in d:
+#		good.append(d)
+#print('一共有', len(good), '筆資料提到good')
